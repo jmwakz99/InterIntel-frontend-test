990ef70cf890889afe181e58367eb36ed460e571
Johnson Mwanjila <kemsjunior99@gmail.com>
2021-02-17 21:06:32 +0300

    converted custom element my-view1 to LitElement

--- src/my-view1.js
+++ src/my-view1.js
@@ -8,26 +8,13 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import {LitElement, html} from 'lit-element';
 import './shared-styles.js';
 
-class MyView1 extends PolymerElement {
-  static get template() {
+class MyView1 extends LitElement {
+  render() {
     return html`
-      <style include="shared-styles">
-        :host {
-          display: block;
-
-          padding: 10px;
-        }
-      </style>
-
-      <div class="card">
-        <div class="circle">1</div>
-        <h1>View One</h1>
-        <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
-        <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
-      </div>
+      <div>View one</div>
     `;
   }
 }
