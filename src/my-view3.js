0266df3c25004a02f19b13b42ec24a194bc85eac
Johnson Mwanjila <kemsjunior99@gmail.com>
2021-02-18 22:11:27 +0300

    fetched todos using fetch api and displayed them using cards

--- src/my-view3.js
+++ src/my-view3.js
@@ -8,28 +8,74 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import {LitElement, html, css } from 'lit-element';
 import './shared-styles.js';
 
-class MyView3 extends PolymerElement {
-  static get template() {
-    return html`
-      <style include="shared-styles">
-        :host {
-          display: block;
+class MyView3 extends LitElement {
+  constructor(){
+    super()
+    this.todos = []
+  }
+  static get properties() {
+    return {
+      todos: {type: Array}
+    
 
-          padding: 10px;
-        }
-      </style>
-
-      <div class="card">
-        <div class="circle">3</div>
-        <h1>View Three</h1>
-        <p>Modus commodo minimum eum te, vero utinam assueverit per eu.</p>
-        <p>Ea duis bonorum nec, falli paulo aliquid ei eum.Has at minim mucius aliquam, est id tempor laoreet.Pro saepe pertinax ei, ad pri animal labores suscipiantur.</p>
-      </div>
-    `;
   }
 }
+  firstUpdated(changedProperties) {
+    super.firstUpdated(changedProperties);
+    fetch(`https://jsonplaceholder.typicode.com/todos?_limit=5`).then(response => {
+      return response.json()
 
-window.customElements.define('my-view3', MyView3);
+    }).then(data => {
+      console.log(data)
+      this.todos = [...data]
+    }).catch(error => {
+      console.log(error)
+    })
+    }
+   render() {
+     return html`
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css"> 
+     <div class="container mt-5 ml-4">
+     <div class="columns is-mobile is-multiline">
+        ${this.todos.map(todo => html`
+        <div class="column">
+        <div class="card" style="width: 14rem;">
+        <header class="card-header">
+          <p class="card-header-title">
+            ${todo.title}
+          </p>
+        
+        </header>
+        <div class="card-content">
+          <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+           
+          </div>
+        </div>
+        <footer class="card-footer is-flex-direction-row is-justify-content-space-between p-3">
+        <span class="tag is-white">Complete</span>
+        <span class="tag pl-2 ${todo.completed ? 'is-success' : 'is-light'}">${todo.completed}</span>
+        
+        </footer>
+        </div>
+    
+         </div>
+        
+        ` )}
+   
+       
+   
+        </div>
+ 
+
+      </div>
+       
+ 
+     `;
+   }
+}
+
+customElements.define('my-view3', MyView3);
